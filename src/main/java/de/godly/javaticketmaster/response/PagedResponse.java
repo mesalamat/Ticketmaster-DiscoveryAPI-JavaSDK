8bf97e085b59781ed5f829425ee038611c4e5597
Godlycrusader <33331056+Godlycrusader@users.noreply.github.com>
2024-03-11 15:12:09 +0100

    Fixed PagedResponse not exposing the PageInfo object via Getter

--- src/main/java/de/godly/javaticketmaster/response/PagedResponse.java
+++ src/main/java/de/godly/javaticketmaster/response/PagedResponse.java
@@ -2,8 +2,11 @@ package de.godly.javaticketmaster.response;
 
 import com.google.gson.annotations.SerializedName;
 import de.godly.javaticketmaster.objects.PageInfo;
+import lombok.Data;
 
+
+@Data
 public abstract class PagedResponse extends DiscoveryResponse {
     @SerializedName("page")
-    private PageInfo pageInfo;
+    private final PageInfo pageInfo;
 }
